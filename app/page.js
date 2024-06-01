6c20d2d2d0fdcd01c095a384e1741d58d5f578e9
rask-kasuterra <kasuterra.524@gmail.com>
2024-06-01 15:23:59 +0900

    Added TicTacToe URL

--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
 import styles from "./page.module.css";
 
 export default function Home() {
+  const ticTacToeUrl = process.env.TIC_TAC_TOE_URL;
+
   return (
     <>
       <head>
@@ -9,6 +11,7 @@ export default function Home() {
       <body>
         <main className={styles.main}>
           <h1>遊び場 GAMES にようこそ！</h1>
+          <a href={ticTacToeUrl}>Go to TicTacToe</a>
         </main>
       </body>
     </>
